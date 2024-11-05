122b01d0dec0ef532ea203200a51fb5b12d562ba
Gleb Avrorin <g.avrorin@gmail.com>
2024-11-05 14:56:07 -0800

    inital push with yaml

--- cypress/e2e/Bank/test.cy.js
+++ cypress/e2e/Bank/test.cy.js
@@ -1,12 +1,5 @@
 describe('Test Number 1', () => {
   
-    it.skip('should allow searching for "Silicon" in the search field', () => {
-      cy.visit('https://www.fdic.gov/bank-failures/failed-bank-list');  
-      cy.get('[name="combine"]').type('Silicon');
-      cy.get('[value="Apply"]').click()
-      cy.get('[headers="view-title-table-column"] a').should('contain.text', 'Silicon').and('have.length', 1);
-    });
-
     it('should validate that only 568 banks are visible', () => {
         cy.visit('https://www.fdic.gov/bank-failures/failed-bank-list');  
         cy.get('[class="form-select usa-select"]').select('All')
