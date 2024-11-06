cfcf1237f92ea61a14aabea4bfa53336a2d47ef7
Gleb Avrorin <g.avrorin@gmail.com>
2024-11-05 17:28:08 -0800

    inital push with yaml

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,8 @@ module.exports = defineConfig({
   reporterOptions: {
     reportDir: "cypress/reports",
     overwrite: false,
-    html: false,
-    json: true,
+    html: true,
+    json: false,
   },
   e2e: {
     setupNodeEvents(on, config) {
